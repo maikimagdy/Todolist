1fec586b6bdc86b9f9e45f9020f02a4a9c698836
Maiki Magdy <maikimagdy6@gmail.com>
2024-02-19 11:50:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Allapp from "./Allapp";
 import ReactPlayer from "react-player";
 import Calc from "./Calculator App/Calc";
-import GoalApp from "./GoalApp/GoalApp";
 
 function App() {
   return (
