c33ab6c3757e3ba20a8fabc6e4733b05b5152db5
maikimagdy <118453771+maikimagdy@users.noreply.github.com>
2024-03-01 22:15:10 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
 import Allapp from "./Allapp";
-import ReactPlayer from "react-player";
-import Calc from "./Calculator App/Calc";
-import GoalApp from "./GoalApp/GoalApp";
 
 function App() {
   return (
